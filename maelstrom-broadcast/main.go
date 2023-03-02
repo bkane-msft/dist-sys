6a0ac78678a95e7971560dca0a19b1698986f500
Benjamin Kane <92759008+bkane-msft@users.noreply.github.com>
2023-03-01 16:31:54 -0800

    broken

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -7,11 +7,42 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+// -- Set
+
+type Set struct {
+	set map[int]struct{}
+}
+
+func NewSet() Set {
+	return Set{
+		set: make(map[int]struct{}),
+	}
+}
+
+func (s *Set) Add(v int) {
+	s.set[v] = struct{}{}
+}
+
+func (s *Set) Contains(v int) bool {
+	_, exists := s.set[v]
+	return exists
+}
+
+func (s *Set) ToSlice() []int {
+	var ret []int
+	for k := range s.set {
+		ret = append(ret, k)
+	}
+	return ret
+}
+
 func main() {
 	n := maelstrom.NewNode()
 
 	// TODO: mutex here?
-	var messages []int
+	var messages = NewSet()
+
+	topology := make(map[string][]string)
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var body map[string]any
@@ -20,7 +51,15 @@ func main() {
 		}
 
 		message := int(body["message"].(float64))
-		messages = append(messages, message)
+
+		if !messages.Contains(message) {
+			messages.Add(message)
+
+			neighbors := n.NodeIDs()
+			for _, neighbor := range neighbors {
+				n.Send(neighbor, msg)
+			}
+		}
 
 		response := map[string]string{
 			"type": "broadcast_ok",
@@ -31,12 +70,28 @@ func main() {
 	n.Handle("read", func(msg maelstrom.Message) error {
 		response := make(map[string]any)
 		response["type"] = "read_ok"
-		response["messages"] = messages
+		response["messages"] = messages.ToSlice()
 
 		return n.Reply(msg, response)
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
+
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		topology_resp := body["topology"].(map[string]any)
+		for node, v := range topology_resp {
+			neigbhors := v.([]any)
+			for _, v := range neigbhors {
+				neighbor := v.(string)
+
+				topology[node] = append(topology[node], neighbor)
+			}
+		}
+
 		response := map[string]string{
 			"type": "topology_ok",
 		}
